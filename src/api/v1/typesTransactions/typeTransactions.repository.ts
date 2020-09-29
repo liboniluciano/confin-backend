8567e02f0df077978194bd4445c17770e0b034b3
Luciano Liboni <luciano_liboni@hotmail.com>
2020-09-29 12:24:29 -0300

    :sparkles: getTypeTransaction on repository

--- src/api/v1/typesTransactions/typeTransactions.repository.ts
+++ src/api/v1/typesTransactions/typeTransactions.repository.ts
@@ -6,4 +6,12 @@ export default class TypeTransactionsRepository extends Repository<TypesTransact
   public async createTypeTransactions(typeTransactions: TypesTransactions): Promise<TypesTransactions> {
     return await this.save(typeTransactions);
   }
+
+  public async getTypeTransactions(): Promise<TypesTransactions[]> {
+    return await this.find();
+  }
+
+  public async getTypeTransaction(typeTransaction: number): Promise<TypesTransactions | undefined> {
+    return this.findOne({ where: {id: typeTransaction }});
+  }
 }
